0a378a752ad7bba1d8a506ebc6dba8534633188b
Dody <dodyags@gmail.com>
2025-02-21 09:34:30 +0700

    feat: initial commit

---
+++ runme.sh
@@ -0,0 +1,4 @@
+spotdl sync "Hot Hits Indonesia.spotdl" --output="{list-name}/{title} - {artists}.{output-ext}" && \
+spotdl sync "Mudik.spotdl" --output="{list-name}/{title} - {artists}.{output-ext}" && \
+spotdl sync "Top 50 - Indonesia.spotdl" --output="{list-name}/{title} - {artists}.{output-ext}" && \
+spotdl sync "Top Songs - Indonesia.spotdl" --output="{list-name}/{title} - {artists}.{output-ext}"
