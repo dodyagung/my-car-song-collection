42a5c2655c2457825983ba2fd42435b50e351a56
Dody <dodyags@gmail.com>
2025-11-16 12:10:35 +0700

    Update 16 nov

--- runme.sh
+++ runme.sh
@@ -1,5 +1,4 @@
 spotdl sync "Hot Hits Indonesia.spotdl" --output="{list-name}/{title} - {artists}.{output-ext}" && \
-spotdl sync "Top 50 - Global.spotdl" --output="{list-name}/{title} - {artists}.{output-ext}" && \
 spotdl sync "Top 50 - Indonesia.spotdl" --output="{list-name}/{title} - {artists}.{output-ext}" && \
-spotdl sync "Top Songs - Global.spotdl" --output="{list-name}/{title} - {artists}.{output-ext}" && \
-spotdl sync "Top Songs - Indonesia.spotdl" --output="{list-name}/{title} - {artists}.{output-ext}"
+spotdl sync "Top Songs - Indonesia.spotdl" --output="{list-name}/{title} - {artists}.{output-ext}" && \
+spotdl sync "Puncak Klasemen.spotdl" --output="{list-name}/{title} - {artists}.{output-ext}"
